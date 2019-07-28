313d86c71db7716d6ec3839a12afa9fa7f950e27
sumitgaur4545 <46048177+sumitgaur4545@users.noreply.github.com>
2019-07-28 18:37:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { AppRegistry, View, Text, } from "react-native";
+
+export default class App extends Component {
+    render() {
+        return (
+        
+            <View >
+                <Text>hello sumit i am ready to use please enjoy today because i am start
+                    today is awesome day because i am use expo
+                </Text>
+                 </View>
+        );
+    }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent("kokoApp", () => App)
